3aea6875c346fc78282e088c21148ae2b0d7d5f9
m.sacramento <migsacramento@gmail.com>
2019-02-26 00:04:37 +0100

    Figure géométrique : suite et fin

--- calcul.js
+++ calcul.js
@@ -7,10 +7,20 @@ public class Rectangle {
 
 public class Carré {
 	public static void main(String [] arg { 
-		system.out.println("Le périmètre d'un carré se calcul en faisant côté*4")
+		system.out.println("Le périmètre d'un carré se calcul en faisant côté*4");
 		System.out.println("La surface s'obtient en faisant côté*côté");
 	
 
 	}
+
+}
+
+public class Cercle {
+        public static void main(String [] arg {
+                system.out.println("Le périmètre du cercle se calcul en faisant : pi*Diamètre avec Diamètre= Rayon*2");
+                System.out.println("La surface s'obtient en faisant: r*r*pi ou r²*pi ");
+
+
+        }
 
 }
