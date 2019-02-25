912f83315e1c2bdcb0c4e771f1ec61e84b82aee4
m.sacramento <migsacramento@gmail.com>
2019-02-25 23:29:50 +0100

    Deuxième figure géométrique

--- calcul.js
+++ calcul.js
@@ -1,7 +1,17 @@
 public class Rectangle {
 	public static void main(String[] arg {
-		system.out.println("Le périmètre du rectangle est égale à L+l")
+		system.out.println("Le périmètre du rectangle est égale à L+l");
 		'Certainement pas une erreur
 		System.out.println("La surface du rectangle est égale à L*l");
 	}
+}
+
+public class Carré {
+	public static void main(String [] arg { 
+		system.out.println("Le périmètre d'un carré se calcul en faisant côté*4")
+		System.out.println("La surface s'obtient en faisant côté*côté");
+	
+
+	}
+
 }
